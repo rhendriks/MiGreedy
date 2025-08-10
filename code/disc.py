49493a205385ddef0613b1a22e47724748603d7e
remi <remi.hendriks@utwente.nl>
2025-08-10 17:14:49 +0200

    improve function

--- code/disc.py
+++ code/disc.py
@@ -34,33 +34,36 @@ class Disc(object):
         
         return (self.haversine_distance(other._latitude,other._longitude)) <= (self.getRadius() + other.getRadius())
 
-    def haversine_distance(self, lat2, lon2):
+    import math
+
+    def haversine_distance(self, lat2: float, lon2: float) -> float:
         """
-        Calculates the great-circle distance between two points on Earth.
+        Calculate the great-circle distance between this disc and another point.
 
         Args:
-            self: The current disc object with latitude and longitude.
-            lat2_rad, lon2_rad: Latitude and longitude of other disc.
+            lat2 (float): Latitude of the other point in radians.
+            lon2 (float): Longitude of the other point in radians.
 
         Returns:
-            float: The distance between the two points in kilometers.
+            float: Distance between points in kilometers.
         """
 
-        # lat1_rad = math.radians(self._latitude)
-        # lon1_rad = math.radians(self._longitude)
-        #
-        # lon2_rad = math.radians(lon2)
-        # lat2_rad = math.radians(lat2)
+        lat1 = self._latitude
+        lon1 = self._longitude
 
-        dlon = lon2 - self._longitude
-        dlat = lat2 - self._latitude
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
 
-        a = np.sin(dlat / 2.0) ** 2 + np.cos(self._latitude) * np.cos(lat2) * np.sin(dlon / 2.0) ** 2
-        c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+        cos_lat1 = math.cos(lat1)
+        cos_lat2 = math.cos(lat2)
+
+        a = (math.sin(dlat / 2) ** 2 +
+             cos_lat1 * cos_lat2 * math.sin(dlon / 2) ** 2)
+
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
         distance = EARTH_RADIUS * c
 
         return distance
-
 
     def __str__(self):
         return "%s\t%s\t%s\t%s\n" % (self._hostname, self._latitude,  self._longitude, self._radius)
